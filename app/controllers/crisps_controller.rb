4acdef1aca67766bb04705e7142fd9b78310dc58
Matthew <cameron.matt@outlook.com>
2021-06-16 18:24:33 +1000

    created new crisp form, and rendering created crisps to index view

--- app/controllers/crisps_controller.rb
+++ app/controllers/crisps_controller.rb
@@ -1,5 +1,6 @@
 class CrispsController < ApplicationController
     def index
+        @crisps = Crisp.all.order("created_at DESC")
     end
 
     def new
@@ -8,6 +9,12 @@ class CrispsController < ApplicationController
 
     def create
         @crisp = Crisp.new(book_params)
+
+        if @crisp.save
+            redirect_to root_path
+        else 
+            render 'new'
+        end
     end
 
     private
