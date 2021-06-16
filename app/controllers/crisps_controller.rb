3305c8c5313314026248f0a7cce4c523fc725d6a
Matthew <cameron.matt@outlook.com>
2021-06-16 16:02:29 +1000

    Added first views

--- app/controllers/crisps_controller.rb
+++ app/controllers/crisps_controller.rb
@@ -1,4 +1,18 @@
 class CrispsController < ApplicationController
     def index
     end
+
+    def new
+        @crisp = Crisp.new
+    end
+
+    def create
+        @crisp = Crisp.new(book_params)
+    end
+
+    private
+
+    def book_params
+        params.require(:crisp).permit(:name)
+    end
 end
