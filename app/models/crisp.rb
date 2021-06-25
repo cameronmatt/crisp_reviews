e58e8ba2708ec31b66147566e5b0b13f83efe683
Matthew <cameron.matt@outlook.com>
2021-06-25 12:17:19 +1000

    Added validation for crisp, reviews and comments.

--- app/models/crisp.rb
+++ app/models/crisp.rb
@@ -2,6 +2,9 @@ class Crisp < ApplicationRecord
     belongs_to :user
     has_many :reviews
 
+    validates :name, :crisp_img, presence: true
+    validates :name, uniqueness: true
+
     has_attached_file :crisp_img, styles: { crisp_index: "250x350>", crisp_show: "325x475>" }, default_url: "/images/:style/missing.png"
     validates_attachment_content_type :crisp_img, content_type: /\Aimage\/.*\z/
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -3,6 +3,8 @@ class Review < ApplicationRecord
     belongs_to :user
     has_many :comments
 
+    validates :rating, :comment, presence: true
+
     scope :three_star, -> { where(rating: 5) }
     scope :three_star, -> { where(rating: 4) }
     scope :three_star, -> { where(rating: 3) }
